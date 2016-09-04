9d150795d5749a0572a87ccfcf866e02d947242f
kozyilmaz <kazim@monolytic.com>
2016-09-05 00:52:23 +0300

    download when necessary

--- gcc.sh
+++ gcc.sh
@@ -2,7 +2,8 @@
 
 #
 # Where do you want to install gcc?
-# WARNING: selected path will be suffixed with gcc version!
+# WARNING: selected path will be suffixed with
+#          gcc version, like installdir-gcc-x.x.x!
 #
 export GCC_INSTALL_DIRECTORY=/usr/local
 
@@ -10,14 +11,12 @@ export GCC_INSTALL_DIRECTORY=/usr/local
 #
 # Set temporary paths
 #
-export path_to_build=$PWD/tmp/builddir
-export path_to_source=$PWD/tmp/srcdir
-export path_to_install=$PWD/tmp/installdir
+export path_to_build=$PWD/tmp/build
+export path_to_source=$PWD/tmp/src
+export path_to_install=$PWD/tmp/install
 mkdir -p $path_to_build
 mkdir -p $path_to_source
 mkdir -p $path_to_install
-
-export PATH=$path_to_install/bin:$PATH
 
 
 #
@@ -30,8 +29,6 @@ ISL_VERSION="0.16.1"
 GCC_VERSION="6.2.0"
 
 PARALLEL_JOBS=4
-PACKAGE_DOWNLOAD=y
-PACKAGE_BUILD=y
 
 GMP_SELECT=y
 MPFR_SELECT=y
@@ -39,42 +36,39 @@ MPC_SELECT=y
 ISL_SELECT=y
 GCC_SELECT=y
 
+
 #
 # Download packages
 #
-if [ $PACKAGE_DOWNLOAD = y ]; then
-  if [ $GMP_SELECT = y ]; then
-    echo "Download gmp-$GMP_VERSION"
-    cd $path_to_source; curl -OL https://gmplib.org/download/gmp-$GMP_VERSION/gmp-$GMP_VERSION.tar.bz2
-  fi
-  if [ $MPFR_SELECT = y ]; then
-    echo "Download mpc-$MPC_VERSION"
-    cd $path_to_source; curl -OL https://ftp.gnu.org/gnu/mpc/mpc-$MPC_VERSION.tar.gz
-  fi
-  if [ $MPC_SELECT = y ]; then
-    echo "Download mpfr-$MPFR_VERSION"
-    cd $path_to_source; curl -OL https://ftp.gnu.org/gnu/mpfr/mpfr-$MPFR_VERSION.tar.bz2
-  fi
-  if [ $ISL_SELECT = y ]; then
-    echo "Download isl-$ISL_VERSION"
-    cd $path_to_source; curl -OL ftp://gcc.gnu.org/pub/gcc/infrastructure/isl-$ISL_VERSION.tar.bz2
-  fi
-  if [ $GCC_SELECT = y ]; then
-    echo "Download gcc-$GCC_VERSION"
-    cd $path_to_source; curl -OL https://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.bz2
-  fi
-  echo "Download complete!"
+if [ $GMP_SELECT = y ] && [ ! -f $path_to_source/gmp-$GMP_VERSION.tar.bz2 ]; then
+  echo "Download gmp-$GMP_VERSION"
+  cd $path_to_source; curl -OL https://gmplib.org/download/gmp-$GMP_VERSION/gmp-$GMP_VERSION.tar.bz2
 fi
+if [ $MPFR_SELECT = y ] && [ ! -f $path_to_source/mpfr-$MPFR_VERSION.tar.bz2 ]; then
+  echo "Download mpfr-$MPFR_VERSION"
+  cd $path_to_source; curl -OL https://ftp.gnu.org/gnu/mpfr/mpfr-$MPFR_VERSION.tar.bz2
+fi
+if [ $MPC_SELECT = y ] && [ ! -f $path_to_source/mpc-$MPC_VERSION.tar.gz ]; then
+  echo "Download mpc-$MPC_VERSION"
+  cd $path_to_source; curl -OL https://ftp.gnu.org/gnu/mpc/mpc-$MPC_VERSION.tar.gz
+fi
+if [ $ISL_SELECT = y ] && [ ! -f $path_to_source/isl-$ISL_VERSION.tar.bz2 ]; then
+  echo "Download isl-$ISL_VERSION"
+  cd $path_to_source; curl -OL ftp://gcc.gnu.org/pub/gcc/infrastructure/isl-$ISL_VERSION.tar.bz2
+fi
+if [ $GCC_SELECT = y ] && [ ! -f $path_to_source/gcc-$GCC_VERSION.tar.bz2 ]; then
+  echo "Download gcc-$GCC_VERSION"
+  cd $path_to_source; curl -OL https://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.bz2
+fi
+echo "Download complete!"
 
-#
-# Install xcode reqs
-#
-xcode-select --install
 
 #
 # Build packages
 #
-if [ $PACKAGE_BUILD = y ]; then
+# get required bins and a fresh start
+xcode-select --install
+rm -rf $path_to_install/*
 
 #
 # GMP
@@ -138,9 +132,9 @@ cd $path_to_build/gcc-$GCC_VERSION
 mkdir build && cd build
 ../configure --prefix=$GCC_INSTALL_DIRECTORY/gcc-$GCC_VERSION --enable-checking=release --with-gmp=$path_to_install --with-mpfr=$path_to_install --with-mpc=$path_to_install --enable-languages=c,c++ --with-isl=$path_to_install --program-suffix=-$GCC_VERSION
 make -j $PARALLEL_JOBS
+echo "Enter password to complete installation to $GCC_INSTALL_DIRECTORY"
 make install
-fi
-
 echo "gcc installed to $GCC_INSTALL_DIRECTORY"
 fi
+
 
